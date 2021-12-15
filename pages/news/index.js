019b5b04f5dcb1e2ca3a7b92314db5b6b47f3f0f
Simon Calle Laverde <kyverde88@gmail.com>
2021-12-15 15:02:18 -0500

    Update index.js
    --- Video/Tutorial 51:00.

--- pages/news/index.js
+++ pages/news/index.js
@@ -3,10 +3,12 @@
 import Link from "next/link";
 import { Fragment } from "react";
 
+// List Could Normally Be Generated Dynamically By Maping Some Array Of Data Into JSX Elements
 export default function NewsPage() {
 	return (
 		<Fragment>
 			<h1>The News Page</h1>
+
 			<ul>
 				<li>
 					<Link href="/news/nextjs-is-a-great-framework-article">
@@ -14,7 +16,11 @@ export default function NewsPage() {
 					</Link>
 				</li>
 				
-				<li>Something Else Article</li>
+				<li>
+					<a href="/news/something-else-article">
+						Something Else Article
+					</a>
+				</li>
 			</ul>
 		</Fragment>
 	);
