a8a7c70c63a8e35d1ba8ff98ac6e3405efe54c01
Simon Calle Laverde <kyverde88@gmail.com>
2021-12-15 13:29:11 -0500

    Update [newsArticles].js
    --- Video/Tutorial 51:00.

--- pages/news/[newsArticles].js
+++ pages/news/[newsArticles].js
@@ -5,10 +5,10 @@ import { useRouter } from "next/router";
 export default function DetailPage() {
 	const routerObject = useRouter();
 
+	// This Will Hold The Concrete Value In The URL For The Dynamic Segment Of The Page Visited
 	const newsArticles = routerObject.query.newsArticles;
 
-	// Send A Request To The Backend API
-	// To Fetch The News Item With "newsArticles"
+	// We Could Send A Request To The Backend API To Fetch The News Item With "newsArticles"
 
 	return <h1>The Detail Page</h1>
 };
